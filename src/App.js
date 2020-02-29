f6abe85cf5b98201e0ebed26fa2734d7e2cf2659
Fábio Moreira <fabiommmoreira@gmail.com>
2020-02-29 16:09:14 +0000

    Add Apollo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
 import logo from './logo.svg';
 import './App.css';
 
+const GET_DOCUMENT = gql`
+  {
+    share(shortId: "Y8wDM") {
+      shortId
+      version {
+        document {
+          name
+          artboards {
+            entries {
+              name
+              isArtboard
+              files {
+                url
+                height
+                width
+                scale
+                thumbnails {
+                  url
+                  height
+                  width
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(GET_DOCUMENT);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log('data', data);
+
   return (
     <div className="App">
       <header className="App-header">
